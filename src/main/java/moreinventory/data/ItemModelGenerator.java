bb433b59aad2cac8e74e42585d5f09a6cb31a1d0
KGT511 <kougyokutei511@gmail.com>
2022-10-30 20:12:36 +0900

    レシピやモデルなど
    (cherry picked from commit b84ba271adca367876a11e79492cc06ab58d9248)

--- src/main/java/moreinventory/data/ItemModelGenerator.java
+++ src/main/java/moreinventory/data/ItemModelGenerator.java
@@ -38,6 +38,7 @@ public class ItemModelGenerator extends ItemModelProvider {
         singleTexTool(Blocks.EXPORTER.get());
         singleTexTool(Items.SPANNER.get());
         registerPouch();
+        registerPlating();
     }
 
     public static String name(Block block) {
@@ -110,5 +111,18 @@ public class ItemModelGenerator extends ItemModelProvider {
         }
         singleTexTool(Items.POUCH.get());
         singleTexTool(Items.LEATHER_PACK.get());
+    }
+
+    public void registerPlating() {
+        singleTexTool(Items.BRUSH.get());
+        singleTexTool(Items.IRON_PLATING.get());
+        singleTexTool(Items.GOLD_PLATING.get());
+        singleTexTool(Items.DIAMOND_PLATING.get());
+        singleTexTool(Items.EMERALD_PLATING.get());
+        singleTexTool(Items.COPPER_PLATING.get());
+        singleTexTool(Items.TIN_PLATING.get());
+        singleTexTool(Items.BRONZE_PLATING.get());
+        singleTexTool(Items.SILVER_PLATING.get());
+        singleTexTool(Items.STEEL_PLATING.get());
     }
 }
